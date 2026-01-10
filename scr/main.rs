f051408db3d899dc0fc9b54041ce04a56f0ca69c
st9405996-gif <st9405996@gmail.com>
2026-01-11 03:06:23 +0500

    Initialize OMNI Ghost Protocol in main.rs

---
+++ scr/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("OMNI Ghost Protocol v5.0 Initializing...");
+    println!("Node Status: Active | Mode: Decentralized Mesh");
+    // Initializing P2P stack for Karachi Pilot
+}
